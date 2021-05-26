5cfe7454f45241ab6f17e72c001403825b09a965
YogeshNain2015 <32641131+YogeshNain2015@users.noreply.github.com>
2021-05-26 11:10:09 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,4 +7,4 @@ sudo apt-get install python3 -y
 sudo apt-get install apache2 libapache2-mod-wsgi-py3 -y
 python3 -m pip install --upgrade pip
 python3 -m pip install --user virtualenv
-pip install --user --upgrade virtualenv
+pip3 install --user --upgrade virtualenv
